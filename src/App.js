f17a936c9f6aa2c53f6bc2dcbd55d5652cbc6160
vigneshtestengineer <vigneshkv6533@gmail.com>
2024-02-04 09:33:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login/Login';
+import Dashboard from './Dashboard/Dash'; // Create a Dashboard component
+import Products from './Products/products';
+import Addproducts from './Products/add-product';
+import Accounts from './Accounts/accounts';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/add-products' element={<Addproducts/>}/>
+      <Route path='/accounts' element={<Accounts/>}/>
+    </Routes>
+  </Router>
   );
-}
+};
 
 export default App;
